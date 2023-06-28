cd7f42168d27832d79287a4e66492be055aedd61
aryanrahul1313 <130580694+aryanrahul1313@users.noreply.github.com>
2023-06-28 15:15:08 +0530

    Find Circular Linked list.cpp

---
+++ Linked List/Circular Linked list.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h> 
+/*************************************************
+        Following is the structure of class Node:
+     
+        class Node{
+        public:
+            int data;
+            Node* next;
+            
+            Node(int data){
+                this->data = data;
+                this->next = NULL;
+            }
+            
+        }
+**************************************************/
+
+bool isCircular(Node* head){
+    // Write your code here.
+}
