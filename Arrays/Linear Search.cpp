b8b4c1d82f7257bcd99baec3257bce69e4052c33
aryanrahul1313 <130580694+aryanrahul1313@users.noreply.github.com>
2023-07-25 22:20:17 +0530

    Linear Search.cpp

---
+++ Arrays/Linear Search.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+bool search(int arr[], int size, int key){
+    for(int i=0;i<size;i++){
+        if(arr[i]==key){
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int main(){
+   int num[50] = {1,3,5,7,9,-2,-4,-6,-8,0};
+   int key;
+   cout<<"Enter the key ";
+   cin>>key;
+
+   bool found = search(num,50,key);
+   if(found){
+    cout<<"Key is found";
+   }
+   else
+    cout<<"Key not found";
+
+   return 0;
+}
