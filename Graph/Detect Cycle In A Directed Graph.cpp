b1c2f6347a2769fc2c1637339a0a992881492d52
aryanrahul1313 <130580694+aryanrahul1313@users.noreply.github.com>
2023-09-03 19:00:13 +0530

    Update Detect Cycle In A Directed Graph.cpp

--- Graph/Detect Cycle In A Directed Graph.cpp
+++ Graph/Detect Cycle In A Directed Graph.cpp
@@ -1,3 +1,65 @@
+//Approach 1: Using DFS
+
+#include<bits/stdc++.h>
+
+bool checkCycleDFS(unordered_map<int, bool> &visited, unordered_map<int, bool> &dfsVisited,
+                    unordered_map<int, list<int>> &adj, int node){
+
+    visited[node] = true;
+    dfsVisited[node] = true;
+
+    for(auto neighbour : adj[node]){
+      if(!visited[neighbour]){
+        bool cycleDetected = checkCycleDFS(visited, dfsVisited, adj, neighbour);
+        if(cycleDetected){
+          return true;
+        }
+      }
+      else if(dfsVisited[neighbour]){
+        return true;
+      }
+    }
+
+    dfsVisited[node] = false;
+    return false;                    
+}
+
+int detectCycleInDirectedGraph(int n, vector <pair<int, int>> &edges)
+{
+  // create adjacency list
+  unordered_map<int, list<int>> adj;
+
+  for(int i=0; i<edges.size(); i++){
+    int u = edges[i].first;
+    int v = edges[i].second;
+
+    adj[u].push_back(v); 
+  }
+
+  // call dfs for all component
+  unordered_map<int, bool> visited;
+  unordered_map<int, bool> dfsVisited;
+
+  for(int i=1; i<=n; i++){
+    if(!visited[i]){
+      bool cycleFound = checkCycleDFS(visited, dfsVisited, adj, i);
+      if(cycleFound){
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+
+
+
+
+
+
+
+//Approach 2: Using BFS
+
 #include<bits/stdc++.h>
 
 int detectCycleInDirectedGraph(int n, vector <pair<int, int>> &edges)
