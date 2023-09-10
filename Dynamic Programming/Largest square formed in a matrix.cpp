ed8a577b83f86e0c7fd07fbe804c4d5263a1c03a
aryanrahul1313 <130580694+aryanrahul1313@users.noreply.github.com>
2023-09-11 00:14:10 +0530

    Create Largest square formed in a matrix.cpp

---
+++ Dynamic Programming/Largest square formed in a matrix.cpp
@@ -0,0 +1,105 @@
+//Approach 1 : Recursion + Memoization
+class Solution{
+public:
+    int solve(vector<vector<int>> &mat, int i, int j, int &maxi, vector<vector<int>> &dp)
+    {
+        //base case
+        if(i >= mat.size() || j >= mat[0].size()){
+            return 0;
+        }
+        
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        
+        int right = solve(mat, i, j+1, maxi, dp);
+        int diagonal = solve(mat, i+1, j+1, maxi, dp);
+        int down = solve(mat, i+1, j, maxi, dp);
+        
+        if(mat[i][j] == 1){
+            dp[i][j] = 1 + min(right, min(diagonal, down));
+            maxi = max(maxi, dp[i][j]);
+            return dp[i][j];
+        }
+        else{
+            return dp[i][j] = 0;
+        }
+    }
+
+    int maxSquare(int n, int m, vector<vector<int>> mat){
+        int maxi = 0;
+        vector<vector<int>> dp(n, vector<int> (m, -1));
+        solve(mat, 0, 0, maxi, dp);
+        return maxi;
+    }
+};
+
+******************************************************************************
+//Approach 2 : Tabulation
+class Solution{
+public:
+    int solve(vector<vector<int>> &mat, int &maxi){
+        int row = mat.size();
+        int col = mat[0].size();
+        vector<vector<int>> dp(row+1, vector<int> (col+1, 0));
+        
+        for( int i=row-1; i>=0; i--){
+            for(int j=col-1; j>=0; j--){
+                int right = dp[i][j+1];
+                int diagonal = dp[i+1][j+1];
+                int down = dp[i+1][j];
+                
+                if(mat[i][j] == 1){
+                    dp[i][j] = 1 + min(right, min(diagonal, down));
+                    maxi = max(maxi, dp[i][j]);
+                }
+                else{
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        return dp[0][0];
+    }
+
+    int maxSquare(int n, int m, vector<vector<int>> mat){
+        int maxi = 0;
+        solve(mat, maxi);
+        return maxi;
+    }
+};
+
+*******************************************************************************
+//Approach 3 : Space Optimization
+class Solution{
+public:
+    int solve(vector<vector<int>> &mat, int &maxi){
+        int row = mat.size();
+        int col = mat[0].size();
+        vector<int> curr(col+1, 0);
+        vector<int> next(col+1, 0);
+        
+        for( int i=row-1; i>=0; i--){
+            for(int j=col-1; j>=0; j--){
+                int right = curr[j+1];
+                int diagonal = next[j+1];
+                int down = next[j];
+                
+                if(mat[i][j] == 1){
+                    curr[j] = 1 + min(right, min(diagonal, down));
+                    maxi = max(maxi, curr[j]);
+                }
+                else{
+                    curr[j] = 0;
+                }
+            }
+            next = curr;
+        }
+        return next[0];
+    }
+
+    int maxSquare(int n, int m, vector<vector<int>> mat){
+        int maxi = 0;
+        solve(mat, maxi);
+        return maxi;
+    }
+};
